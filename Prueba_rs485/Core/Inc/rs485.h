1fbdc1d6633c8a8c8ccfe47e7070bd4c0306972a
Juan Leithold <juan.leithold@gmail.com>
2023-09-29 13:08:21 -0300

    Primera version de comunicación RS485. No funcional

---
+++ Prueba_rs485/Core/Inc/rs485.h
@@ -0,0 +1,16 @@
+#ifndef INC_RS485_H_
+#define INC_RS485_H_
+
+#define BUFFER_TX 64
+#define TIME_OUT 100
+
+#define LEN_BUF 4
+#define LEN_DATA 5
+#define DELIMITER ','
+
+void RS485_Send_Buffer(char* str);
+void RS485_Flush_Buffer(void);
+uint8_t RS485_Read_Buffer(char buf[LEN_BUF][LEN_DATA]);
+uint8_t RS485_Read_Address(char const* add_device);
+
+#endif
